0c1a65bfa4e26829f43f25552c030473447c8b05
noohn <noohn@10.127.50.131>
2025-06-10 11:17:48 -0400

    added next page

--- src/icsculminating/MySketch.java
+++ src/icsculminating/MySketch.java
@@ -15,6 +15,11 @@ import processing.core.PImage;
     
 public class MySketch extends PApplet {
     private PImage backgroundImg;
+    private PImage homePage;
+    private PImage learnButton;
+    private int learnX,learnY;
+    private PImage playButton;
+    
     private int stage =0;
     //private Car car2;
     //private boolean showInfo=false;
@@ -25,14 +30,30 @@ public class MySketch extends PApplet {
     }
     public void setup(){
         backgroundImg = loadImage("images/start.png");
+        homePage = loadImage("images/home.png");
+        learnButton = loadImage("images/learnButton.png");
+        playButton = loadImage("images/playButton.png");
         //car = new Car(this,300,100,"images/car.png",1);
         //car2 = new Car(this,100,100,"images/car.png",2);
     }
     public void draw(){
         if (stage==0){
+            //showing the start page
             background(backgroundImg);
-        } else{
+        //home page
+        } else if (stage==1){
+            //showing the home page
+            background(homePage);
+            //showing the buttons
+            learnX = 270;
+            learnY=420;
+            this.image(learnButton,270,420);
+            this.image(playButton,270,500);
+        
+        //learning page
+        } else if (stage==2){
             background(0);
+            System.out.print("Learn");
         }
         
         //car.draw();
@@ -52,17 +73,24 @@ public class MySketch extends PApplet {
 //            carClicked.displayInfo(this);
 //        }
     }
-    public boolean isClicked(int mouseX, int mouseY, PImage image, int radius){
-        int centerX=(image.pixelWidth/2);
-        int centerY=(image.pixelHeight/2);
+    public boolean isClicked(int mouseX, int mouseY, PImage image, int x, int y){
+        //if (image!=null){
+           int centerX=x+(image.pixelWidth/2);
         
-        float d =PApplet.dist(mouseX,mouseY, centerX,centerY);
-        return d<radius;
+           int centerY= y+(image.pixelHeight/2);
+           float d =PApplet.dist(mouseX,mouseY, centerX,centerY);
+           float r = image.pixelWidth/2;
+           System.out.println("Raidus: "+r+" Distance: "+d);
+           return d<r;
     }
     
     public void mousePressed(){
-        if(isClicked(mouseX,mouseY,backgroundImg,350)){
+        //if start screen is clicked, move to home screen 
+        if(isClicked(mouseX,mouseY,backgroundImg,350,350)){
             stage=1;
+        } else if (isClicked(mouseX,mouseY,learnButton,learnX,learnY)){
+            //learning page
+            stage =2;
         }
     }
 }
