5bde47ad23d28bb8f8dbfd785791dd55509abc73
noohn <noohn@10.127.50.131>
2025-06-13 00:41:46 -0400

    added learninh pages

--- src/icsculminating/MySketch.java
+++ src/icsculminating/MySketch.java
@@ -17,6 +17,12 @@ public class MySketch extends PApplet {
     private PImage backgroundImg;
     private PImage welcomePage;
     private PImage namePage;
+    private PImage learn1;
+    private PImage learn2;
+    private PImage learn3;
+    private PImage learn4;
+    private PImage proceedPage;
+    
     
     private int stage =0;
     private String name= "";
@@ -31,13 +37,25 @@ public class MySketch extends PApplet {
         backgroundImg = loadImage("images/start.png");
         welcomePage = loadImage("images/welcomePage.png");
         namePage = loadImage("images/namePage.png");
+        //loading learning pages
+        learn1 = loadImage("images/learn1.png");
+        learn2 = loadImage("images/learn2.png");
+        learn3 = loadImage("images/learn3.png");
+        learn4 = loadImage("images/learn4.png");
+        proceedPage = loadImage("images/proceedPage.png");
+        //setting my textsize 
+        textSize(60);
     }
     
     public void keyPressed(){
-       if (stage==1&&keyCode==ENTER){
+       if (stage!=7&&keyCode==ENTER){
            stage++;
-       } else if(key!=CODED&&stage==1){
-           name+=key;
+       } else if(key!=CODED&&stage==2){
+           if(key==BACKSPACE){
+               name = name.substring(0,name.length()-1);
+           } else{
+             name+=key;  
+           }
        }
     }
     
@@ -45,27 +63,47 @@ public class MySketch extends PApplet {
         background(255);
         if (stage==0){
             //showing the start page
+            background(255);
             image(backgroundImg,0,0);
         //home page
-        } if (stage==1){
+        } else if (stage==1){
             //clear();
             //showing the home page
             image(welcomePage,0,0);
             //showing the buttons
         
         //learning page
-        } if (stage==2){
-            //clear();
+        } else if (stage==2){
             image(namePage,0,0);
-        }
-        if (keyPressed){
-            if (keyCode==ENTER){
-                stage++;
-                System.out.println("Next stage");
+            //setting text
+            fill(0);
+            text(name,300,355);
+        
+//            if (keyPressed){
+//                if (keyCode==ENTER){
+//                    keyPressed =false;
+//                    stage++;
+//                    System.out.println("Next stage");
+//                }
+//            }
+        //START OF LEARNING SECTION
+        } else if(stage==3){
+            image(learn1,0,0);
             
+        } else if(stage==4){
+            image(learn2,0,0);
+            
+        } else if(stage==5){
+            image(learn3,0,0);
+            
+        }else if(stage==6){
+            image(learn4,0,0);
+        
+        //proceed page
+        }else if(stage==7){
+            image(proceedPage,0,0);
         }
-    }
-    }
+    }  
     public boolean isClicked(int mouseX, int mouseY, PImage image, int x, int y){
         if (image != null) {
             int imageWidth = image.width;
@@ -80,7 +118,7 @@ public class MySketch extends PApplet {
     
     public void mousePressed(){
         //if start screen is clicked, move to home screen 
-        if(isClicked(mouseX,mouseY,backgroundImg,0,0)){
+        if(stage==0 && isClicked(mouseX,mouseY,backgroundImg,0,0)){
             stage=1;
     }
 }
